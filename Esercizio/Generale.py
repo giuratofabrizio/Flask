617ebc4b3c2d28ffe0185b39c4738b3cf6cb72d4
giuratofabrizio <giurato.fabrizio@iisgalvanimi.edu.it>
2022-02-24 10:51:16 +0000

    Esercizio 1.1

--- Esercizio/Generale.py
+++ Esercizio/Generale.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+import random
 
 @app.route('/')
 def Gen():
   return render_template('Generale.html')
+
+
+@app.route('/meteo')
+def meteo():
+    nRandom = random.randint(0,8)
+    if nRandom <= 2:
+        immagine = "static/Pioggia.jpg"
+    elif nRandom <= 5:
+        immagine = "static/nuvole.jpg"
+    else:
+        immagine = "static/sole.jpg"
+    return render_template("meteo.html", meteo=immagine)
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
