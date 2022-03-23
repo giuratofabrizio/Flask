0f17f4db0092f5cb2e5d1d797fc8143b5f3d4cdc
giuratofabrizio <giurato.fabrizio@iisgalvanimi.edu.it>
2022-03-23 13:54:58 +0000

    Fontanelle

--- AppEs6/AppEs6.py
+++ AppEs6/AppEs6.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template, send_file, make_response, url_for, Response
+#realizzare un sito web che restituisca la mappa dei quartieri di milano.
+#ci deve essere una homepage con un link "quartieri di milano":
+#cliccando su questo link si deve visualizzare la mappa dei quartieri di milano 
+
+
+from flask import Flask, render_template, send_file, make_response, url_for, Response, request
 app = Flask(__name__)
 
 import io
@@ -10,16 +15,25 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-
-milano = gpd.read_file('/workspace/Flask/AppEs6/ds964_nil_wm-20220322T104009Z-001.zip')
-
-
+milano = gpd.read_file('/workspace/Flask/AppEs6/static/ds964_nil_wm-20220322T104009Z-001.zip')
+print(milano)
 
 @app.route('/', methods=['GET'])
-def simple():
-    return render_template('simple.html')
+def home():
+    return render_template('home.html')
 
 
+
+
+
+
+
+
+
+@app.route('/visualizza', methods=['GET'])
+def visualizza():
+    return render_template('plot.html')
+    
 
 @app.route('/plot.png', methods=['GET'])
 def plot_png():
@@ -33,11 +47,58 @@ def plot_png():
     return Response(output.getvalue(), mimetype='image/png')
 
 
-
 @app.route('/plot', methods=("POST", "GET"))
 def mpl():
     return render_template('plot.html',
                            PageTitle = "Matplotlib")
-                           
+
+
+
+
+
+
+
+@app.route('/trova', methods=['GET'])
+def trova():
+    return render_template('str.html')
+
+@app.route('/ricerca', methods = ['GET'])
+def ricerca():
+    
+    quartiere = request.args['Quartiere']
+    global risultato
+    risultato = milano[milano['NIL']==quartiere.upper()]
+
+    if len(risultato) == 0:
+        table = 'quaritere non trovato'
+        return render_template('table.html', tabella = table)
+    else:
+        table = risultato.to_html()
+        #caricamento immagine e visualizzazione
+        return render_template('plot2.html')
+
+
+        
+@app.route('/plot2.png', methods=['GET'])
+def plot2_png():
+
+    fig, ax = plt.subplots(figsize = (12,8))
+
+    risultato.to_crs(epsg=3857).plot(ax=ax, alpha=0.5)
+    contextily.add_basemap(ax=ax)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/scelta', methods=("POST", "GET"))
+def plot35():   
+    qrt = milano.NIL.to_list()
+    qrt.sort()
+    return render_template('menu.html', qrt=qrt)
+
+
+
+
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
